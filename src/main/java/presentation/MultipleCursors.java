6b36ae2880ea17b565dbf6e54e590eb2e6cd57c0
pavel.yurkin <pavel.yurkin@leroymerlin.ru>
2019-01-14 19:17:59 +0300

    first commit

---
+++ src/main/java/presentation/MultipleCursors.java
@@ -0,0 +1,33 @@
+package presentation;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MultipleCursors {
+
+    public void init() {
+        List<String> cards = new ArrayList();
+
+    }
+
+    /*
+     * ♠J
+     * ♠D
+     * ♠K
+     * ♠A
+     * ♥J
+     * ♥D
+     * ♥K
+     * ♥A
+     * ♦J
+     * ♦D
+     * ♦K
+     * ♦A
+     * ♣J
+     * ♣D
+     * ♣K
+     * ♣A
+     */
+}
---
+++ src/main/java/presentation/NavigationInsideClass.java
@@ -0,0 +1,50 @@
+package presentation;
+
+import java.io.File;
+
+public class NavigationInsideClass {
+
+    public void method(String path) {
+        if (path == null) {
+            throw new IllegalArgumentException("Input path be null");
+        }
+        String toUpperCase = path.trim();
+
+        System.out.println(toUpperCase);
+
+
+        /*
+         * Some code
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         */
+
+        StringBuilder stringBuilder = new StringBuilder(toUpperCase);
+        stringBuilder.append(System.getProperty("line.separator"));
+        stringBuilder.append("\t");
+        stringBuilder.append(toUpperCase);
+
+        String concat = toUpperCase.concat(stringBuilder.toString());
+        System.out.println(concat);
+
+        String file = toUpperCase + ".xml";
+        boolean delete = new File(file).delete();
+        System.out.println("File deletion: " + delete);
+    }
+
+}
